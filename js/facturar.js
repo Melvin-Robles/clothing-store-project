a9ee027b4fa122a8708589d21403a418d47cdc72
RoblesDeveloper! <eduarobles200152@gmail.com>
2024-03-01 14:44:19 -0600

    fix

---
+++ js/facturar.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const product = JSON.parse(sessionStorage.getItem('productsInCart'));
+
+    const invoiceBody = document.getElementById('invoiceBody');
+    let totalAmount = 0;
+
+    product.forEach(producto => {
+        const subtotal = producto.precio * producto.cantidad;
+        totalAmount += subtotal;
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${producto.nombreProducto}</td>
+            <td>${producto.precio}</td>
+            <td>${producto.cantidad}</td>
+            <td>${subtotal.toFixed(2)}</td>
+        `;
+        invoiceBody.appendChild(row);
+    });
+
+    document.getElementById('totalAmount').textContent = totalAmount.toFixed(2);
+
+    document.getElementById('exportButton').addEventListener('click', function() {
+    var invoiceHtml = document.getElementById('invoiceDiv').innerHTML;
+
+    var newWindow = window.open('', '');
+    newWindow.document.head.innerHTML = `
+    <script src="js/facturar.js"></script>
+    <link rel="stylesheet" href="css/general.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@422&display=swap" rel="stylesheet">
+
+    `;
+    newWindow.document.body.innerHTML = invoiceHtml;
+    setTimeout(() => {
+        newWindow.print();
+    }, 1100);
+});
+
+    document.getElementById("returnButton").addEventListener("click", function() {
+  window.history.back();
+});
+});
